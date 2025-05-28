b064c6cbd203fbb00d7a48cde796efb7e9355ff7
wifon <kaziorigor0902@gmail.com>
2025-05-29 01:33:22 +0200

    Update Countdown.jsx

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -1,61 +1,41 @@
 import { useEffect, useState } from "react";
 
 const Countdown = ({ targetDate }) => {
-  const [timeLeft, setTimeLeft] = useState({
-    days: 0,
-    hours: 0,
-    minutes: 0,
+  const [time, setTime] = useState({
     seconds: 0,
+    minutes: 0,
+    hours: 0,
+    days: 0,
   });
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const IntervalId = setInterval(() => {
       const now = new Date();
       const target = new Date(targetDate);
       const difference = target.getTime() - now.getTime();
 
       if (difference <= 0) {
-        clearInterval(interval);
-        setTimeLeft({
-          days: 0,
-          hours: 0,
-          minutes: 0,
+        setTime({
           seconds: 0,
+          minutes: 0,
+          hours: 0,
+          days: 0,
         });
         return;
       }
 
-      setTimeLeft({
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / (1000 * 60)) % 60),
+      setTime({
         seconds: Math.floor((difference / 1000) % 60),
+        minutes: Math.floor((difference / (1000 * 60)) % 60),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
       });
     }, 1000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(IntervalId);
   }, [targetDate]);
 
-  return (
-    <div className="flex gap-4 text-center text-white">
-      <div>
-        <div className="text-4xl font-bold">{timeLeft.days}</div>
-        <div className="text-sm uppercase">Dni</div>
-      </div>
-      <div>
-        <div className="text-4xl font-bold">{timeLeft.hours}</div>
-        <div className="text-sm uppercase">Godziny</div>
-      </div>
-      <div>
-        <div className="text-4xl font-bold">{timeLeft.minutes}</div>
-        <div className="text-sm uppercase">Minuty</div>
-      </div>
-      <div>
-        <div className="text-4xl font-bold">{timeLeft.seconds}</div>
-        <div className="text-sm uppercase">Sekundy</div>
-      </div>
-    </div>
-  );
+  return <div>{time.seconds}</div>;
 };
 
 export default Countdown;
