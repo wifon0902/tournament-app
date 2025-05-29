951f2c7870cc9307559a37a8b576d6b8139db3c0
wifon <kaziorigor0902@gmail.com>
2025-05-30 01:32:24 +0200

    feat: new button style

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -22,6 +22,8 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
         accent:
           "bg-main-accent hover:bg-main-accent-hover text-white cursor-pointer ",
+        transparent:
+          "border bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 cursor-pointer",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
